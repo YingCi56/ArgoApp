2eb758f61e48ed62837cf617f63e5e4e272a02a6
Ying Ci <yingci5609@gmail.com>
2024-06-03 04:49:50 +0800

    edits

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    html = f"<h1 style='color:{os.environ.get('FONT_COLOR')}'>{os.environ.get('CUSTOM_HEADER')}</h1>"
+    html = f"{os.environ.get('CUSTOM_HEADER')}</h1>"
     return html
