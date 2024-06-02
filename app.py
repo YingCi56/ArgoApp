d00b8615bd1d0a550c3a8ec5b69049be10a8e2e1
Ying Ci <yingci5609@gmail.com>
2024-06-03 05:53:45 +0800

    gitignore

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    html = f"<h1>{os.environ.get('CUSTOM_HEADER')}</h1><h2>Hey</h2>"
+    html = f"<h1>{os.environ.get('CUSTOM_HEADER')}"
     return html
